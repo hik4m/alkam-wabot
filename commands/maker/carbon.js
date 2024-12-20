e8930585b6a7b14f436f99188e1a36657fced849
ahqliza <123288409+hik4m@users.noreply.github.com>
2024-12-20 15:02:14 +0700

    [FIX] Edited endpoint API siputzx

--- commands/maker/carbon.js
+++ commands/maker/carbon.js
@@ -1,40 +1,44 @@
-const {
-    quote
-} = require("@mengkodingan/ckptw");
+const { quote } = require("@mengkodingan/ckptw");
 const mime = require("mime-types");
 
 module.exports = {
-    name: "carbon",
-    aliases: ["carbonify"],
-    category: "maker",
-    handler: {
-        coin: [10, "text", 1]
-    },
-    code: async (ctx) => {
-        if (await handler(ctx, module.exports.handler)) return;
+  name: "carbon",
+  aliases: ["carbonify"],
+  category: "maker",
+  handler: {
+    coin: [10, "text", 1],
+  },
+  code: async (ctx) => {
+    if (await handler(ctx, module.exports.handler)) return;
 
-        const input = ctx.args.join(" ") || null;
+    const input = ctx.args.join(" ") || null;
 
-        if (!input) return await ctx.reply(
-            `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
-            quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, 'console.log("halo dunia!");'))
-        );
+    if (!input)
+      return await ctx.reply(
+        `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
+          quote(
+            tools.msg.generateCommandExample(
+              ctx._used.prefix + ctx._used.command,
+              'console.log("halo dunia!");'
+            )
+          )
+      );
 
-        try {
-            const apiUrl = tools.api.createUrl("brat", "/carbon", {
-                code: input
-            });
+    try {
+      const apiUrl = tools.api.createUrl("siputzx", "/api/m/carbonify", {
+        input: input,
+      });
 
-            return await ctx.reply({
-                image: {
-                    url: apiUrl
-                },
-                mimetype: mime.lookup("png")
-            });
-        } catch (error) {
-            console.error(`[${config.pkg.name}] Error:`, error);
-            if (error.status !== 200) return await ctx.reply(config.msg.notFound);
-            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
-        }
+      return await ctx.reply({
+        image: {
+          url: apiUrl,
+        },
+        mimetype: mime.lookup("png"),
+      });
+    } catch (error) {
+      console.error(`[${config.pkg.name}] Error:`, error);
+      if (error.status !== 200) return await ctx.reply(config.msg.notFound);
+      return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
     }
+  },
 };
