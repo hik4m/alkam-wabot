cd10c3646bac610a5eb65dcbfd4385bf726f1c38
ahqliza <123288409+hik4m@users.noreply.github.com>
2024-12-20 15:00:19 +0700

    Update donate.js

--- commands/information/donate.js
+++ commands/information/donate.js
@@ -1,23 +1,20 @@
-const {
-    quote
-} = require("@mengkodingan/ckptw");
+const { quote } = require("@mengkodingan/ckptw");
 
 module.exports = {
-    name: "donate",
-    aliases: ["donasi"],
-    category: "information",
-    handler: {},
-    code: async (ctx) => {
-        if (await handler(ctx, module.exports.handler)) return;
+  name: "donate",
+  aliases: ["donasi"],
+  category: "information",
+  handler: {},
+  code: async (ctx) => {
+    if (await handler(ctx, module.exports.handler)) return;
 
-        return await ctx.reply(
-            `${quote("083838039693 (DANA)")}\n` +
-            `${quote("─────")}\n` +
-            `${quote("https://paypal.me/itsreimau (PayPal)")}\n` +
-            `${quote("https://saweria.co/itsreimau (Saweria)")}\n` +
-            `${quote("https://trakteer.id/itsreimau (Trakteer)")}\n` +
-            "\n" +
-            config.msg.footer
-        ); // Dapat diubah sesuai keinginan Anda
-    }
+    return await ctx.reply(
+      `${quote("085733326427 (DANA)")}\n` +
+        `${quote("───────────────")}\n` +
+        `${quote("https://saweria.co/hikamramadhan (Saweria)")}\n` +
+        `${quote("https://trakteer.id/aqlizahikam (Trakteer)")}\n` +
+        "\n" +
+        config.msg.footer
+    ); // Dapat diubah sesuai keinginan Anda
+  },
 };
