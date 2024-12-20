4eb587a2b4896cc01f0cc1f319873fe95d59be83
ahqliza <123288409+hik4m@users.noreply.github.com>
2024-12-20 15:01:00 +0700

    Update tqto.js

--- commands/information/tqto.js
+++ commands/information/tqto.js
@@ -1,27 +1,28 @@
-const {
-    quote
-} = require("@mengkodingan/ckptw");
+const { quote } = require("@mengkodingan/ckptw");
 
 module.exports = {
-    name: "tqto",
-    aliases: ["thanksto"],
-    category: "information",
-    handler: {},
-    code: async (ctx) => {
-        if (await handler(ctx, module.exports.handler)) return;
+  name: "tqto",
+  aliases: ["thanksto"],
+  category: "information",
+  handler: {},
+  code: async (ctx) => {
+    if (await handler(ctx, module.exports.handler)) return;
 
-        return await ctx.reply(
-            `${quote("Allah SWT")}\n` +
-            `${quote("ItsReimau (https://github.com/itsreimau)")}\n` +
-            `${quote("JastinXyz (https://github.com/JastinXyz)")}\n` +
-            `${quote("Idul (https://github.com/aidulcandra)")}\n` +
-            `${quote("UdeanDev (https://github.com/udeannn)")}\n` +
-            `${quote("ZTRdiamond (https://github.com/ZTRdiamond)")}\n` +
-            `${quote("FastURL (alias Hikaru) (https://github.com/fasturl)")}\n` +
-            `${quote("Serv00 (https://serv00.com/)")}\n` +
-            `${quote("Dan kepada semua pihak yang telah membantu dalam pengembangan bot ini.")}\n` +
-            "\n" +
-            config.msg.footer
-        ); // Jika Anda tidak menghapus ini, terima kasih!
-    }
+    return await ctx.reply(
+      `${quote("Allah SWT")}\n` +
+        `${quote("HikamRmdhn (https://github.com/hik4m)")}\n` +
+        `${quote("ItsReimau (https://github.com/itsreimau)")}\n` +
+        `${quote("JastinXyz (https://github.com/JastinXyz)")}\n` +
+        `${quote("Idul (https://github.com/aidulcandra)")}\n` +
+        `${quote("UdeanDev (https://github.com/udeannn)")}\n` +
+        `${quote("ZTRdiamond (https://github.com/ZTRdiamond)")}\n` +
+        `${quote("FastURL (alias Hikaru) (https://github.com/fasturl)")}\n` +
+        `${quote("Serv00 (https://serv00.com/)")}\n` +
+        `${quote(
+          "Dan kepada semua pihak yang telah membantu dalam pengembangan bot ini."
+        )}\n` +
+        "\n" +
+        config.msg.footer
+    ); // Jika Anda tidak menghapus ini, terima kasih!
+  },
 };
