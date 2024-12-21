1d4d9bc8949b69b946b4a22fcc7e86ee44d90705
ahqliza <123288409+hik4m@users.noreply.github.com>
2024-12-21 18:34:00 +0700

    Create cekorean.js

---
+++ commands/random/cekorean.js
@@ -0,0 +1,45 @@
+const { quote } = require("@mengkodingan/ckptw");
+const axios = require("axios");
+const mime = require("mime-types");
+
+module.exports = {
+    name: "cekorean",
+    category: "random",
+    handler: {
+        coin: 10,
+    },
+    code: async (ctx) => {
+        if (await handler(ctx, module.exports.handler)) return;
+
+        // URL API
+        const apiUrl = tools.api.createUrl("btch", `/korea`);
+
+        try {
+            // Fetch data gambar dari API
+            const response = await axios.get(apiUrl, {
+                responseType: "arraybuffer",
+            });
+
+            const buffer = Buffer.from(response.data); 
+         
+            if (!buffer || buffer.length === 0) {
+                return await ctx.reply(quote(`⚠️ Gambar tidak ditemukan.`));
+            }
+
+
+            await ctx.reply({
+                image: buffer, 
+                mimetype: mime.lookup("png"), 
+                caption: "Ini hasil gambarnya !", 
+            });
+        } catch (error) {
+            console.error(`[${config.pkg.name}] Error:`, error);
+
+            
+            if (error.response && error.response.status !== 200) {
+                return await ctx.reply(config.msg.notFound);
+            }
+            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+        }
+    },
+};
