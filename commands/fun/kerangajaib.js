74fe2bf55ae01a278948bdf3185043d3ac115ed1
ahqliza <123288409+hik4m@users.noreply.github.com>
2024-12-23 15:19:55 +0700

    Create kerangajaib.js

---
+++ commands/fun/kerangajaib.js
@@ -0,0 +1,33 @@
+const { quote } = require("@mengkodingan/ckptw");
+
+module.exports = {
+    name: "kerangajaib",
+    category: "fun",
+    handler: {
+     coin: 10,
+    },
+
+    code: async (ctx) => {
+        const input = ctx.args.join(" ") || null;
+
+        if (!input) return await ctx.reply(
+            `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
+            quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, "john doe"))
+        );
+        
+        const answers = [
+            "Kurasa tidak",
+            "Tidak",
+            "Tidak juga",
+            "Bisa jadi",
+            "Mungkin",
+            "Iya",
+            "Sudah pasti",
+            "Sudah tentu"
+        ];
+
+        const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+        await ctx.reply(quote(`Pertanyaan: ${input}\nKerang Ajaib: *${randomAnswer}*`));
+    }
+};
