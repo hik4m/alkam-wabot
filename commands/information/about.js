4ab5071ddc8dbe71595d9a573833c1adbdbf2322
ahqliza <123288409+hik4m@users.noreply.github.com>
2024-12-24 19:44:14 +0700

    [Add] Owner 2

--- commands/information/about.js
+++ commands/information/about.js
@@ -10,7 +10,7 @@ module.exports = {
         if (await handler(ctx, module.exports.handler)) return;
 
         return await ctx.reply(
-            quote(`👋 Halo! Saya adalah Bot WhatsApp bernama ${config.bot.name}, dimiliki oleh ${config.owner.name}. Saya bisa melakukan banyak perintah, seperti membuat stiker, menggunakan AI untuk pekerjaan tertentu, dan beberapa perintah berguna lainnya. Saya di sini untuk menghibur dan menyenangkan Anda!`)
+            quote(`👋 Halo! Saya adalah Bot WhatsApp bernama ${config.bot.name}, dimiliki oleh ${config.owner.name} dan ${config.owner2.name} beserta tim ${config.owner.organization}. Saya bisa melakukan banyak perintah, seperti membuat stiker, menggunakan AI untuk pekerjaan tertentu, dan beberapa perintah berguna lainnya. Saya di sini untuk menghibur dan menyenangkan Anda!`)
         ); // Dapat diubah sesuai keinginan Anda
     }
 };
