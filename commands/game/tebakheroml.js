1056fc90e003bed4cceadf3e92c338e9946d645c
ahqliza <123288409+hik4m@users.noreply.github.com>
2025-01-10 12:39:36 +0700

    Update tebakheroml.js

--- commands/game/tebakheroml.js
+++ commands/game/tebakheroml.js
@@ -10,7 +10,9 @@ const session = new Map();
 module.exports = {
     name: "tebakheroml",
     category: "game",
-    handler: {},
+    handler: {
+     group: true, 
+},
     code: async (ctx) => {
         if (await handler(ctx, module.exports.handler)) return;
 
